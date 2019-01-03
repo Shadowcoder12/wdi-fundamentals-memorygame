2fcb5ce37b7ed6c784949a6ee166f89642d1fb3b
Leonardo Fontenette <lfontenette@Leonardos-MacBook-Pro-2.local>
2019-01-03 01:18:10 -0800

    Created JS Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
+var cards = ["queen","queen", "king", "king"];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("YOU HAVE FOUND A MATCH!!");
+	}
+	else {
+		alert("Sorry, try again!")};
+};
+
 
 console.log("User flipped " + cardOne);
+
+console.log("User flipped " + cardTwo);
