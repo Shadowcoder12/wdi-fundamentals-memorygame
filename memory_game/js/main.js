ce83951c5ee61d9706970f64ace97642e904b1cb
Leonardo Fontenette <lfontenette@Leonardos-MacBook-Pro-2.local>
2019-01-03 23:57:47 -0800

    Refractored JS logic code

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,22 @@
-var cardsInPlay = [];
 var cards = ["queen","queen", "king", "king"];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+var cardsInPlay = [];
+var checkForMatch = function () {
+	if(cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("YOU HAVE FOUND A MATCH!!");
+		}
+		else {
+			alert("Sorry, try again!")};
+		};
+	};
+	var flipCard = function (cardId){
+		console.log("User flipped " + cards[cardId]);
+		cardsInPlay.push(cards[cardId]);
+		checkForMatch();
+	};
 
-if(cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("YOU HAVE FOUND A MATCH!!");
-	}
-	else {
-		alert("Sorry, try again!")};
-};
 
 
-console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardTwo);
+
+
