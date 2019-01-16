b0e8283b429ebc22624389e2f1bfb87c2c8d12f3
Leonardo Fontenette <lfontenette@Leonardos-MacBook-Pro-2.local>
2019-01-15 18:44:03 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,31 +1,34 @@
+// Creates an array that stores all card objects
 var cards = [
 {
-rank: "queen",
-suit: "hearts",
-cardImage:"images/queen-of-hearts.png"
+	rank: "queen",
+	suit: "hearts",
+	cardImage:"images/queen-of-hearts.png"
 },
 {
-rank: "queen",
-suit: "diamonds",
-cardImage:"images/queen-of-diamonds.png"
+	rank: "queen",
+	suit: "diamonds",
+	cardImage:"images/queen-of-diamonds.png"
 },
 
 {
-rank: "king",
-suit: "hearts",
-cardImage:"images/king-of-hearts.png"
+	rank: "king",
+	suit: "hearts",
+	cardImage:"images/king-of-hearts.png"
 },
 {
-rank: "king",
-suit: "diamonds",
-cardImage:"images/king-of-diamonds.png"
+	rank: "king",
+	suit: "diamonds",
+	cardImage:"images/king-of-diamonds.png"
 },
 
 ];
 
-
-
+// Creates an array that stores the cards in play
 var cardsInPlay = [];
+
+/* Function that checks if two cards match and if two cards
+ are played */ 
 var checkForMatch = function () {
 	if(cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]){
@@ -35,13 +38,31 @@ var checkForMatch = function () {
 			alert("Sorry, try again!")};
 		};
 	};
-	var flipCard = function (cardId){
+	// function that mimics a user picking a card
+	var flipCard = function (){
+		var cardId = this.getAttribute("data-id");
 		console.log("User flipped " + cards[cardId].rank);
 		console.log(cards[cardId].cardImage);
 		console.log(cards[cardId].suit);
 		cardsInPlay.push(cards[cardId].rank);
+		this.setAttribute('src', cards[cardId].cardImage);
 		checkForMatch();
 	};
+
+	// creates the board 
+	var createBoard = function(){
+		var boardGame = document.getElementById("game-board");
+		for (var i = 0; i < cards.length; i++) {
+			var cardElement = document.createElement("img");
+			cardElement.setAttribute("src", "images/back.png");
+			cardElement.setAttribute("data-id", i);
+			cardElement.addEventListener("click", flipCard); 
+			boardGame.appendChild(cardElement); 
+		}
+	};
+
+	createBoard();
+
 
 
 
